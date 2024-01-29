97502bb39ee4810af4cad2d589130a397dab6a90
Johny Docker <john241985@outlook.com>
2024-01-29 23:12:01 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-function App() {
-  return (
-    <div style={{ textAlign:"center" }}><h1>Simple React App!</h1></div>
-  );
-}
+import React from 'react';
+const App = () => (
+  <>
+    <div class="navbar">
+      <div class="logo"></div>
+    </div>
 
+    <div class="main">
+      <div class="content">
+        <h2>Dashboard</h2>
+      </div>
+    </div>
+  </>
+);
 export default App;
