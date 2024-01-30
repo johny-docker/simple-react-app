139f760cd9bdd4e26d821c740f0987c850233b63
Johny Docker <john241985@outlook.com>
2024-01-30 23:07:49 +0530

    aas

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 const App = () => (
-  <>
-    <div class="navbar">
-      <div class="logo"></div>
-    </div>
+  <div>
+    <nav class="bg-gray-900">
+      <div class="px-4 max-w-7xl sm:px-6 lg:px-8">
+        <div class="flex items-center justify-between h-16">
+          <div class="flex items-center">
+            <div class="flex-shrink-0">
+              <div class="logo justify-center w-1/5 text-center">
 
-    <div class="main">
-      <div class="content">
-        <h2>Dashboard</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <div class="px-6 lg:px-10">
+      <div class="flex items-center justify-between px-10 mt-6 -mx-10 border-b select-none md:mt-4">
+        <div class="flex items-center px-4 pb-2 text-sm border-b-2 border-red-700 cursor-pointer">Dashboard</div>
+      </div>
+      <div class="m-5">
+        sas
       </div>
     </div>
-  </>
+  </div>
 );
 export default App;
